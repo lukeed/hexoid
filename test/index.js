a5cd8576edfb356efc787acc9866d00b71327a3b
Luke Edwards <luke.edwards05@gmail.com>
2020-10-29 10:30:47 -0700

    chore: swap test runner

--- test/index.js
+++ test/index.js
@@ -1,85 +1,57 @@
-import test from 'tape';
+import { test } from 'uvu';
+import * as assert from 'uvu/assert';
 import hexoid from '../src';
 
-test('exports', t => {
-	t.is(typeof hexoid, 'function', 'exports function');
-	t.end();
+test('exports', () => {
+	assert.type(hexoid, 'function', 'exports function');
 });
 
 
-test('returns', t => {
+test('returns', () => {
 	let output = hexoid();
-	t.is(typeof output, 'function', 'returns a function');
-	t.is(typeof output(), 'string', '~> returns a string');
-	t.is(output().length, 16, '~> has 16 characters (default)');
-	t.end();
+	assert.type(output, 'function', 'returns a function');
+	assert.type(output(), 'string', '~> returns a string');
+	assert.is(output().length, 16, '~> has 16 characters (default)');
 });
 
 
-test('length :: 8', t => {
+test('length :: 8', () => {
+	let i=0, tmp;
 	let gen = hexoid(8);
-	let i=0, tmp, bool=true;
-
 	for (; i < 1e3; i++) {
 		tmp = gen();
-		bool = bool && tmp.length === 8;
-		if (!bool) {
-			t.fail(`"${tmp}" is not 8 characters!`);
-			break;
-		}
+		assert.is(tmp.length, 8, `"${tmp}" is not 8 characters!`);
 	}
-
-	t.true(bool, '~> produced 1000 IDs w/ 8 chars each');
-
-	t.end();
 });
 
 
-test('length :: 24', t => {
+test('length :: 24', () => {
+	let i=0, tmp;
 	let gen = hexoid(24);
-	let i=0, tmp, bool=true;
-
 	for (; i < 1e3; i++) {
 		tmp = gen();
-		bool = bool && tmp.length === 24;
-		if (!bool) {
-			t.fail(`"${tmp}" is not 24 characters!`);
-			break;
-		}
+		assert.is(tmp.length, 24, `"${tmp}" is not 24 characters!`);
 	}
-
-	t.true(bool, '~> produced 1000 IDs w/ 24 chars each');
-
-	t.end();
 });
 
 
-test('length :: 48', t => {
+test('length :: 48', () => {
+	let i=0, tmp;
 	let gen = hexoid(48);
-	let i=0, tmp, bool=true;
-
 	for (; i < 1e3; i++) {
 		tmp = gen();
-		bool = bool && tmp.length === 48;
-		if (!bool) {
-			t.fail(`"${tmp}" is not 48 characters!`);
-			break;
-		}
+		assert.is(tmp.length, 48, `"${tmp}" is not 48 characters!`);
 	}
-
-	t.true(bool, '~> produced 1000 IDs w/ 48 chars each');
-
-	t.end();
 });
 
 
-test('unique', t => {
+test('unique', () => {
 	let gen = hexoid();
-	t.not(gen(), gen(), '~> single');
+	assert.is.not(gen(), gen(), '~> single');
 
 	let items = new Set();
 	for (let i=5e6; i--;) items.add(gen());
-	t.is(items.size, 5e6, '~> 5,000,000 uniques');
-
-	t.end();
+	assert.is(items.size, 5e6, '~> 5,000,000 uniques');
 });
+
+test.run();

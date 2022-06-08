e59066432beea3504b7d38bfdd16823f62fc2d02
uzlopak <aras.abbasi@googlemail.com>
2022-06-08 23:37:04 +0200

    fix odds

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ while (i<256) HEX[i]=(256+i++).toString(16).substring(1);
 
 export function hexoid(len) {
 	len||(len=16);
-	var str='',lh =((1+len)>>1)-1,num=lh;
+	var str='',odd=len&1,l2=len-2,lh =((1+len)>>1)-1,num=lh;
 	while (num--) str += HEX[256*Math.random()|0];
+	odd && (str = str.substring(0, l2))
 	return function () {
 		if (num === 255) {
 			str=''; num=lh;
 			while (num--) str += HEX[256*Math.random()|0];
+			odd && (str = str.substring(0, l2));
 		}
 		return str + HEX[++num];
 	};
